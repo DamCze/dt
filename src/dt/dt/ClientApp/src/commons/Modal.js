d2f42e593b054736c28aba955a31b1101c8fe8c3
Damian Czech <damian5k6l@gmail.com>
2019-06-17 22:15:38 +0200

    Dodanie modala

---
+++ src/dt/dt/ClientApp/src/commons/Modal.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Modal from "@material-ui/core/Modal";
+
+export const DietModal = props => (
+  <Modal open={props.open} onBackdropClick={props.handleClose}>
+    <div style={styles.modal}>
+      <button onClick={props.handleClose}>click</button>
+    </div>
+  </Modal>
+);
+
+const styles = {
+  modal: {
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    position: "absolute",
+    width: window.innerWidth / 1.6,
+    height: window.innerHeight / 1.6,
+    flex: 1,
+    backgroundColor: "white",
+    outline: "none"
+  }
+};
--- src/dt/dt/ClientApp/src/diet/view/ExpansionPanelContent.js
+++ src/dt/dt/ClientApp/src/diet/view/ExpansionPanelContent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   ExpansionPanel,
   ExpansionPanelDetails,
@@ -10,31 +10,45 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import { translate } from "react-i18next";
+import { DietModal } from "../../commons/Modal";
 
 const ADD = "add";
 const SAVE = "save";
 
-const ExpansionPanelContent = props => {
-  const { t } = props;
+class ExpansionPanelContent extends Component {
+  state = {
+    isVisible: false
+  };
 
-  return (
-    <ExpansionPanel>
-      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography style={styles.heading}>{t(props.meal)}</Typography>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails style={styles.details}>
-        {/* tutaj tabelka */}
-      </ExpansionPanelDetails>
-      <Divider />
-      <ExpansionPanelActions>
-        <Button size="small">{t(ADD)}</Button>
-        <Button size="small" color="primary">
-          {t(SAVE)}
-        </Button>
-      </ExpansionPanelActions>
-    </ExpansionPanel>
-  );
-};
+  setIsVisible = () => {
+    this.setState(state => ({ isVisible: !state.isVisible }));
+  };
+
+  render() {
+    const { t, meal } = this.props;
+    return (
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography style={styles.heading}>{t(meal)}</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails style={styles.details}>
+          {/* tutaj tabelka */}
+          <DietModal
+            open={this.state.isVisible}
+            handleClose={this.setIsVisible}
+          />
+        </ExpansionPanelDetails>
+        <Divider />
+        <ExpansionPanelActions>
+          <Button size="small">{t(ADD)}</Button>
+          <Button size="small" color="primary" onClick={this.setIsVisible}>
+            {t(SAVE)}
+          </Button>
+        </ExpansionPanelActions>
+      </ExpansionPanel>
+    );
+  }
+}
 
 const styles = {
   heading: {
